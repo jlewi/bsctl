095966cdc7dc2e541b5633d29211de51d1101eaf
Jeremy Lewi <jeremy@lewi.us>
2024-11-30 07:18:46 -0800

    Dump lists.

--- cmd/root.go
+++ cmd/root.go
@@ -23,6 +23,7 @@ func NewRootCmd() *cobra.Command {
 	rootCmd.AddCommand(NewConfigCmd())
 	rootCmd.AddCommand(NewApplyCmd())
 	rootCmd.AddCommand(NewDumpCmd())
+	rootCmd.AddCommand(NewDumpListCmd())
 	rootCmd.AddCommand(NewMergeCmd())
 	rootCmd.AddCommand(NewTidyCmd())
 	rootCmd.AddCommand(NewDumpStarterPack())
